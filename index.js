fe5c08dc59002495bd3bc7019f070b34e0565106
Max <mack@example.com>
2023-03-01 20:55:25 +0400

    inex.js trim

--- index.js
+++ index.js
@@ -3,14 +3,13 @@ import {trimString, getCountOfWords, getCountUniqueWords, setFirstLatterUpperCas
 import * as bi from './bigIntOperations.js';
 
 
+console.log(setFirstLatterUpperCase("pppppppPPP"));
+let str = "Вот пример строки,в которой     используются знаки препинания.После знаков должны стоять пробелы , а перед знаками их быть не должно .    Если есть лишние подряд идущие пробелы, они должны быть устранены."
+console.log(trimString(str));
+console.log(getCountOfWords(str));
 
-// let str = "Вот пример строки,в которой     используются знаки препинания.После знаков должны стоять пробелы , а перед знаками их быть не должно .    Если есть лишние подряд идущие пробелы, они должны быть устранены."
-// let  s = trimString(str);
-// let c = getCountOfWords(str);
+console.log(getCountUniqueWords("Текст, в котором слово текст()$$ несколько раз встречается и слово тоже"));
 
-//let  u = getCountUniqueWords("Текст, в котором слово текст несколько раз встречается и слово тоже")
-//let s = setFirstLatterUpperCase("pppppppPPP")
 
-//let num = bi.subtraction(1209999, 900000)
-let s = getCountUniqueWords("Текст:), в котором слово текст несколько раз встречается и \"слово\" тоже")
-console.log(s)
+console.log(bi.subtraction(1209999, 900000))
+
